a1fe529402136038ca10af8372886c9feb1c9985
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-09 19:03:11 +0000

    add_task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from models import storage
 from flask import Flask
+from flask import make_response, jsonify
 
 # flask instance
 app = Flask(__name__)
@@ -17,6 +18,11 @@ def teardown_storage(exce):
     ''' Calls close from storage '''
     storage.close()
 
+
+@app.errorhandler(404)
+def four_four_Error(e):
+    ''' handle 404 Error '''
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(
