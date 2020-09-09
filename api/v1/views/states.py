661a86541f2cc064ecd6e3824f85e3e7bcd31c0d
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-09 22:19:15 +0000

    add_file

---
+++ api/v1/views/states.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" Handle Api for states"""
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, make_response, abort, request
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    '''retrieve all states'''
+    dict = {}
+    listx = []
+    for obj in storage.all(State).values():
+        listx.append(obj.to_dict())
+    return jsonify(listx)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'],
+                 strict_slashes=False)
+def retrieve_state_id(state_id):
+    """retrieve state with id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_specific_state(state_id):
+    """delete method"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_method():
+    """post methods"""
+
+    if not request.is_json or not request.get_json():
+        abort(400, desciption="Not a JSON")
+    if not "name" in request.get_json():
+        abort(400, description="Missing name")
+    new_state = State(**request.get_json())
+    new_state.save()
+    return make_response(jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """update a state"""
+
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.is_json or not request.get_json():
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    for key, value in data.items():
+        if key != "id" and key != "created_at" and key != "updated_at":
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
