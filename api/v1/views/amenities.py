9bc892efa03b421915ff510a8c0b49b1817ec105
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-10 01:28:09 +0000

    add

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -58,7 +58,7 @@ def update_amenity_obj(amenity_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    new_instance = storage.get("Amenity", amenity_id)
+    new_instance = storage.get(Amenity, amenity_id)
     for key, val in request.get_json().items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(new_instance, key, val)
