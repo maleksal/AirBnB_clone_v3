e9af007689dfd9755407bb335df3f0c114196ec8
Malek Salem <malek.salem.14@gmail.com>
2020-09-10 01:40:39 +0100

    update code structure

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -49,29 +49,41 @@ def create_city(state_id):
 
 @app_views.route(
             "/cities/<city_id>",
-            methods=["GET", "PUT", "DELETE"],
-            strict_slashes=False,)
+            methods=["GET"], strict_slashes=False,)
 def RetrieveUpdateDelete_city(city_id):
-    ''' Retrieves a City object '''
+    ''' retrieve a City object by id '''
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    # Handle GET request
-    if request.method == "GET":
-        return jsonify(city.to_dict())
-    # Handle DELETE request
-    if request.method == "DELETE":
-        storage.delete(city)
-        storage.save()
-        return make_response(jsonify({}), 200)
-    # Handle PUT request
-    if request.method == "PUT":
-        ignore_keys = ("id", "state_id", "created_at", "updated_at")
-        http_request = request.get_json()
-        if not http_request:
-            abort(400, "Not a JSON")
-        for attr, value in http_request.items():
-            if value not in ignore_keys:
-                setattr(city, attr, value)
-        storage.save()
-        return make_response(jsonify(city), 200)
+    return jsonify(city.to_dict())
+
+
+@app_views.route(
+            "/cities/<city_id>",
+            methods=["DELETE"], strict_slashes=False,)
+def delete_city(city_id):
+    """ delete a city object by id """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route(
+            "/cities/<city_id>",
+            methods=["PUT"], strict_slashes=False,)
+def update_city(city_id):
+    ''' update city object by id '''
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    http_request = request.get_json()
+    if not http_request:
+        abort(400, "Not a JSON")
+    for attr, value in http_request.items():
+        if value not in ["id", "state_id", "created_at", "updated_at"]:
+            setattr(city, attr, value)
+    storage.save()
+    return make_response(jsonify(city), 200)
